cb8d6bca3053539c722a45cee62743b941759495
Alexandre Ribes <ribes.alexandre@gmail.com>
2015-07-21 22:22:19 +0200

    Initial commit

---
+++ app/Controllers/Admin/PrivateController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PrivateController.php
+ * ------------
+ *
+ * @author  :  RIBES Alexandre
+ * @contact : ribes.alexandre@gmail.com
+ * @website : http://www.alexandre-ribes.fr
+ */
+
+namespace app\Controllers\Admin;
+
+
+class PrivateController
+{
+	
+}
---
+++ app/Controllers/PublicController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PublicController.php
+ * ------------
+ *
+ * @author  :  RIBES Alexandre
+ * @contact : ribes.alexandre@gmail.com
+ * @website : http://www.alexandre-ribes.fr
+ */
+
+namespace app\Controllers;
+
+
+class PublicController
+{
+	
+}
